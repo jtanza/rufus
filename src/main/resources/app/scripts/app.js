c3f35beb1062b856ad309c87cf1df2cc86ab38af
johntanza <jtanza@crimsonhexagon.com>
2017-07-27 18:42:34 -0400

    handle demo/ non authenticated users

--- src/main/resources/app/scripts/app.js
+++ src/main/resources/app/scripts/app.js
@@ -1,4 +1,14 @@
-var app = angular.module('home', ['ngRoute']);
+var app = angular.module('home', ['ngRoute', 'myLoginCheck']);
+
+var login = angular.module('myLoginCheck', [])
+    .factory('$logincheck', function() {
+        return function(userid) {
+            // Perform logical user logging. Check either
+            // by looking at cookies or make a call to server.
+            if (userid > 0) return true;
+            return false;
+        };
+    });
 
 app.config(function($routeProvider) {
     $routeProvider
@@ -44,8 +54,15 @@ app.config(function($routeProvider) {
         });
     });
 
- app.controller('resourceController', ['$scope', '$route', '$http', '$location', function ($scope, $route, $http, $location) {
-    $http.get($route.current.$$route.resource).then(function success(response) {
+ app.controller('resourceController', ['$scope', '$route', '$http', '$location', '$logincheck', function ($scope, $route, $http, $location, $logincheck) {
+     var route = $route.current.$$route.resource;
+     if (!$logincheck(0)) {
+         $scope.loggedin = false;
+         route = route.replace("articles", "public")
+     }
+     
+    $http.get(route).then(function success(response) {
+        $scope.loggedin = false;
         $scope.articles = response.data;
     }, function error(response) {
         $location.path('/error');
@@ -54,37 +71,48 @@ app.config(function($routeProvider) {
     });
  }]);
 
-app.controller('homeController', ['$scope', '$route', '$http', '$location', function ($scope, $route, $http, $location) {
-    $http.get('api/articles/tagStubs').then(function(response) {
-        $scope.tags = response.data;
-    });
+app.controller('homeController', ['$scope', '$route', '$http', '$location', '$logincheck', function ($scope, $route, $http, $location, $logincheck) {
+    if (!$logincheck(0)) {
+        $http.get('api/public/tagStubs').then(function(response) {
+            $scope.tags = response.data;
+        });
+    } else {
+        $http.get('api/articles/tagStubs').then(function(response) {
+            $scope.tags = response.data;
+        });
 
-    $scope.bookmark = function(article) {
-        if (article.bookmark) {
-           $http.post('api/articles/removeBookmark', article).then(function success(response){
-               article.bookmark = false;
-           }, function error(response) {
-               $location.path('/error');
-               $scope.errCode = response.status;
-               $scope.errMessage = response.data.message;
-           });
-        } else {
-           $http.post('api/articles/bookmark', article).then(function(response) {
-                article.bookmark = true;
-           }, function error(response) {
-               $location.path('/error');
-               $scope.errCode = response.status;
-               $scope.errMessage = response.data.message;
-           });
-         }
+        $scope.bookmark = function(article) {
+            if (article.bookmark) {
+                $http.post('api/articles/removeBookmark', article).then(function success(response){
+                    article.bookmark = false;
+                }, function error(response) {
+                    $location.path('/error');
+                    $scope.errCode = response.status;
+                    $scope.errMessage = response.data.message;
+                });
+            } else {
+                $http.post('api/articles/bookmark', article).then(function(response) {
+                    article.bookmark = true;
+                }, function error(response) {
+                    $location.path('/error');
+                    $scope.errCode = response.status;
+                    $scope.errMessage = response.data.message;
+                });
+            }
+        }
     }
  }]);
 
 app.controller('taggedController', function($scope, $http, $routeParams) {
-    $http.get('api/articles/tagged?tag=' + $routeParams.param).then(function(response) {
+    var route = 'api/articles/tagged?tag=';
+    if (!$logincheck(0)) {
+        $scope.loggedin = false;
+        route = route.replace("articles", "public")
+    }
+    $http.get(route + $routeParams.param).then(function(response) {
         $scope.articles = response.data;
     });
-})
+});
 
 app.controller('addFeedsController', function($scope, $http) {
     $scope.add = function(feed) {
@@ -93,4 +121,4 @@ app.controller('addFeedsController', function($scope, $http) {
             console.log(response);
         })
     };
-})
+});
