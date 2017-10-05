fc53b6c9728a2ccfe404058e428febf5be3da34b
johntanza <jtanza@crimsonhexagon.com>
2017-10-05 18:11:18 -0400

    frontend refactor, nix bootstrap, pull in skeleton css add responsive logic to sidebar

--- src/main/java/com/tanza/rufus/feed/FeedConstants.java
+++ src/main/java/com/tanza/rufus/feed/FeedConstants.java
@@ -5,5 +5,5 @@ package com.tanza.rufus.feed;
  */
 public final class FeedConstants {
     public static final int DEFAULT_DOCS_PER_FEED = 3;
-    public static final int MAX_DESCRIP_LEN = 150;
+    public static final int MAX_DESCRIP_LEN = 500;
 }
--- src/main/resources/app/scripts/app.js
+++ src/main/resources/app/scripts/app.js
@@ -1,4 +1,16 @@
 
+//Toggle between adding and removing the "responsive" class to
+//topnav when the user clicks on the icon
+function toggle() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+
 (function() {
 
     //home grown http lib
@@ -76,5 +88,53 @@
     router.resolve();
 
 })();
+
+
+(function (window, document) {
+
+    var layout   = document.getElementById('layout'),
+        menu     = document.getElementById('menu'),
+        menuLink = document.getElementById('menuLink'),
+        content  = document.getElementById('main');
+
+    function toggleClass(element, className) {
+        var classes = element.className.split(/\s+/),
+            length = classes.length,
+            i = 0;
+
+        for(; i < length; i++) {
+            if (classes[i] === className) {
+                classes.splice(i, 1);
+                break;
+            }
+        }
+        // The className is not found
+        if (length === classes.length) {
+            classes.push(className);
+        }
+
+        element.className = classes.join(' ');
+    }
+
+    function toggleAll(e) {
+        var active = 'active';
+
+        e.preventDefault();
+        toggleClass(layout, active);
+        toggleClass(menu, active);
+        toggleClass(menuLink, active);
+    }
+
+    menuLink.onclick = function (e) {
+        toggleAll(e);
+    };
+
+    content.onclick = function(e) {
+        if (menu.className.indexOf('active') !== -1) {
+            toggleAll(e);
+        }
+    };
+
+}(this, this.document));
 
 
